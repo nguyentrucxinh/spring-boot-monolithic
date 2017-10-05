475a1599fc2607e00bd91849e7e6dcde180fc1cd
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-05 21:25:05 +0700

    Add module DTO

---
+++ dto/src/main/java/com/xinhnguyen/dto/BaseDTO.java
@@ -0,0 +1,13 @@
+package com.xinhnguyen.dto;
+
+/**
+ * Created by xinhnguyen on 05/10/2017.
+ */
+public class BaseDTO {
+
+    public interface validateOnCreate {
+    }
+
+    public interface validateOnUpdate {
+    }
+}
