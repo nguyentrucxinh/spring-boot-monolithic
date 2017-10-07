b26fcb6774fc139ac764c9dd8b5b2890f0f0ad38
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-07 21:02:23 +0700

    Add Domain

---
+++ domain/src/main/java/com/xinhnguyen/domain/User.java
@@ -0,0 +1,70 @@
+package com.xinhnguyen.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role"
+            , joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")}
+            , inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private Set<Role> roles;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_permission"
+            , joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")}
+            , inverseJoinColumns = {@JoinColumn(name = "permission_id", referencedColumnName = "id")})
+    private Set<Permission> permissions;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Set<Role> roles, Set<Permission> permissions) {
+        this.id = id;
+        this.name = name;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
