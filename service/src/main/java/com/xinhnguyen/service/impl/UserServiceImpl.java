611c6569e7ff927cbfbfd405eb0ef1a277db807a
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-07 21:18:14 +0700

    Add @Service to service

--- service/src/main/java/com/xinhnguyen/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/xinhnguyen/service/impl/UserServiceImpl.java
@@ -6,9 +6,11 @@ import com.xinhnguyen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
