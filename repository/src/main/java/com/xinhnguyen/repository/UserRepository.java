9db7e48056efb712cfdcaf408406c9939a632863
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-07 21:04:05 +0700

    Add Repository

---
+++ repository/src/main/java/com/xinhnguyen/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.xinhnguyen.repository;
+
+import com.xinhnguyen.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
