7aec5858cbe5f74631de30b0b7f050f7d77c3797
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-08 20:45:21 +0700

    Add postman collection, custom Entity

--- domain/src/main/java/com/xinhnguyen/domain/Role.java
+++ domain/src/main/java/com/xinhnguyen/domain/Role.java
@@ -1,5 +1,7 @@
 package com.xinhnguyen.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
@@ -14,6 +16,7 @@ public class Role implements Serializable {
     @Column
     private String name;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 
