59e9ff5895ab74bcc093d669c2f78d9db9e7db28
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-11 22:09:48 +0700

    Config swagger

--- web/src/main/java/com/xinhnguyen/web/config/SwaggerConfig.java
+++ web/src/main/java/com/xinhnguyen/web/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.xinhnguyen.web.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -11,33 +12,77 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Provides configuration beans for the Swagger API documentation generator.
+ *
+ * @author Xinh Nguyen
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.xinhnguyen.web.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+
+    /**
+     * The project version.
+     */
+    public static final String PROJECT_VERSION = "0.0.1";
+    /**
+     * The project contact name.
+     */
+    public static final String PROJECT_CONTACT_NAME = "Xinh Nguyen";
+    /**
+     * The project contact URL.
+     */
+    public static final String PROJECT_CONTACT_URL = "https://twitter.com/nguyentrucxinh";
+
+    /**
+     * Create a Contact class to be used by Springfox's Swagger API Documentation framework.
+     *
+     * @return A Contact instance.
+     */
+    private Contact contact() {
+        return new Contact(PROJECT_CONTACT_NAME, PROJECT_CONTACT_URL, null);
+    }
+
+    /**
+     * Create an ApiInfo class to be used by Springfox's Swagger API Documentation framework.
+     *
+     * @return An ApiInfo instance.
+     */
+    private ApiInfo apiInfo() {
+
+        // @formatter:off
+        return new ApiInfoBuilder()
+                .title("Project Skeleton for Spring Boot Web Services")
+                .description("The Spring Boot web services starter project provides a foundation "
+                        + "to rapidly construct a RESTful web services application.")
+                .contact(contact())
+                .version(PROJECT_VERSION)
+                .license("api.license")
+                .licenseUrl("api.license.url")
+                .termsOfServiceUrl("api.tos")
+                .build();
+        // @formatter:on
 
     }
 
-    private ApiInfo apiInfo() {
+    /**
+     * Create a Docket class to be used by Springfox's Swagger API Documentation framework. See
+     * http://springfox.github.io/springfox/ for more information.
+     *
+     * @return A Docket instance.
+     */
+    @Bean
+    public Docket docket() {
+        final Predicate<String> paths = PathSelectors.ant("/api/**");
 
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+        // @formatter:off
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xinhnguyen.web.controller"))
+                .paths(paths)
+                .build();
+        // @formatter:on
 
-        apiInfoBuilder.title("api.name");
-        apiInfoBuilder.description("api.description");
-        apiInfoBuilder.version("api.version");
-        apiInfoBuilder.termsOfServiceUrl("api.tos");
-        apiInfoBuilder.contact(
-                new Contact("api.contact.name", "api.contact.url", "api.contact.email"));
-        apiInfoBuilder.license("api.license");
-        apiInfoBuilder.licenseUrl("api.license.url");
-
-        return apiInfoBuilder.build();
     }
 }
