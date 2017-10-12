b96c9730f915bf1d79ecb4f5f2f67dec306f8940
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-12 23:31:01 +0700

    Add CORS and Transaction

--- web/src/main/java/com/xinhnguyen/web/Application.java
+++ web/src/main/java/com/xinhnguyen/web/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.xinhnguyen.repository"})
 @EntityScan(basePackages = {"com.xinhnguyen.domain"})
 @ComponentScan(basePackages = {"com.xinhnguyen.domain", "com.xinhnguyen.repository", "com.xinhnguyen.seeding", "com.xinhnguyen.service", "com.xinhnguyen.web"})
