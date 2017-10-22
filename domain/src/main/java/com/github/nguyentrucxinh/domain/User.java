3bbfbaeb14e8999884c3c0431c57db80413700d0
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-22 11:11:28 +0700

    Add lombok

--- domain/src/main/java/com/github/nguyentrucxinh/domain/User.java
+++ domain/src/main/java/com/github/nguyentrucxinh/domain/User.java
@@ -1,10 +1,13 @@
 package com.github.nguyentrucxinh.domain;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity(name = "users")
 public class User extends BaseEntityAudit {
 
@@ -27,55 +30,5 @@ public class User extends BaseEntityAudit {
     @Column
     private String lastName;
 
-    public User() {
-    }
 
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordSalt() {
-        return passwordSalt;
-    }
-
-    public void setPasswordSalt(String passwordSalt) {
-        this.passwordSalt = passwordSalt;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
