1eb7567d7461f65894e4f82ed7f7784008648249
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-24 22:58:44 +0700

    Move main class to base package

--- web/src/main/java/com/github/nguyentrucxinh/Application.java
+++ web/src/main/java/com/github/nguyentrucxinh/Application.java
@@ -1,17 +1,16 @@
-package com.github.nguyentrucxinh.web;
+package com.github.nguyentrucxinh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+/*
 @EnableJpaRepositories(basePackages = {"com.github.nguyentrucxinh.repository"})
 @EntityScan(basePackages = {"com.github.nguyentrucxinh.domain"})
 @ComponentScan(basePackages = {"com.github.nguyentrucxinh.domain", "com.github.nguyentrucxinh.repository", "com.github.nguyentrucxinh.seeding", "com.github.nguyentrucxinh.service", "com.github.nguyentrucxinh.web"})
+*/
 public class Application {
 
     public static void main(final String... args) {
